c631d16ebe34fa6a91b4949e9016a7b06e8aa8b0
Khrystyna-Mariia Chomko <xrystynachomko@gmail.com>
2026-03-24 20:58:49 +0200

    chore: sync main with develop and remove proxy

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { PostHogProvider } from '@posthog/react'
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2026-01-30',
-}
+}  
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
